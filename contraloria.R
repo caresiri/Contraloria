2369a55a4557b078f2c43d31d0b98836294b9596
caresiri <carlos.r.siri@gmail.com>
2019-10-04 13:12:09 -0600

    commit 3

--- contraloria.R
+++ contraloria.R
@@ -15,64 +15,51 @@ library(dplyr)
 #SIACSICOP1519 <- read_dta("") # Read Stata data into R when needed
 SIACSICOP1519$DESC_BIEN_SERVICIO <- tolower(SIACSICOP1519$DESC_BIEN_SERVICIO) #convert to lower case
 SIACSICOP1519$DESC_BIEN_SERVICIO <- stri_trans_general(SIACSICOP1519$DESC_BIEN_SERVICIO, id = "Latin-ASCII") # eliminate accents
+cleancorpus <- function(a) {
+  corpus <- VCorpus(VectorSource(a$DESC_BIEN_SERVICIO)) #Generación de corpus de Descripción de Bienes y Servicios del SIAC
+  toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x)) #Eliminar caracteres especiales
+  corpus <- tm_map(corpus, toSpace, "/")#Eliminar caracteres especiales
+  corpus <- tm_map(corpus, toSpace, "@")#Eliminar caracteres especiales
+  corpus <- tm_map(corpus, toSpace, "\\|")#Eliminar caracteres especiales
+  corpus <- tm_map(corpus, removeNumbers) # Remover numeros
+  corpus <- tm_map(corpus, removeWords, stopwords("spanish")) # Remover stopwords de español
+  corpus <- tm_map(corpus, removePunctuation) # Remove puntuación
+  corpus <- tm_map(corpus, stripWhitespace) # Eliminate extra white spaces
+  # Remove your own stop word
+  # specify your stopwords as a character vector
+  #corpus <- tm_map(corpus, removeWords, c("blabla1", "blabla2")) 
+  # estos comandos ya fueron ejecutados desde la base de datos, no es necesario repetirlo
+  #corpus <- tm_map(corpus, content_transformer(tolower)) # Convertir texto a minuscula
+  #removeAccents <- content_transformer(function(x) chartr("áéíóú", "aeiou", x))# Remove Accents
+  #corpus <- tm_map(corpus, removeAccents)# Remove Accents
+  # Text stemming
+  #corpus <- tm_map(corpus, stemDocument, language = "spanish")
+  #### Term document Matrix ####
+  dtm <- TermDocumentMatrix(corpus)
+  d0<-d
+  m <- as.matrix(dtm) # as matrix
+  v <- sort(rowSums(m),decreasing=TRUE) # sort by sum
+  d <- data.frame(word = names(v),freq=v) # frequency data frame
+  head(d, 10) #display top 10
+}
 
-corpus <- VCorpus(VectorSource(SIACSICOP1519$DESC_BIEN_SERVICIO)) #Generación de corpus de Descripción de Bienes y Servicios del SIAC
-
-#### Cleaningdata ####
-toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x)) #Eliminar caracteres especiales
-corpus <- tm_map(corpus, toSpace, "/")#Eliminar caracteres especiales
-corpus <- tm_map(corpus, toSpace, "@")#Eliminar caracteres especiales
-corpus <- tm_map(corpus, toSpace, "\\|")#Eliminar caracteres especiales
-corpus <- tm_map(corpus, removeNumbers) # Remover numeros
-corpus <- tm_map(corpus, removeWords, stopwords("spanish")) # Remover stopwords de español
-corpus <- tm_map(corpus, removePunctuation) # Remove puntuación
-corpus <- tm_map(corpus, stripWhitespace) # Eliminate extra white spaces
-
-# Remove your own stop word
-# specify your stopwords as a character vector
-#corpus <- tm_map(corpus, removeWords, c("blabla1", "blabla2")) 
-
-# estos comandos ya fueron ejecutados desde la base de datos, no es necesario repetirlo
-#corpus <- tm_map(corpus, content_transformer(tolower)) # Convertir texto a minuscula
-#removeAccents <- content_transformer(function(x) chartr("áéíóú", "aeiou", x))# Remove Accents
-#corpus <- tm_map(corpus, removeAccents)# Remove Accents
-
-# Text stemming
-#corpus <- tm_map(corpus, stemDocument, language = "spanish")
-
-
-#### Term document Matrix ####
-dtm <- TermDocumentMatrix(corpus)
-m <- as.matrix(dtm) # as matrix
-v <- sort(rowSums(m),decreasing=TRUE) # sort by sum
-d <- data.frame(word = names(v),freq=v) # frequency data frame
-head(d, 10) #display top 10
+cleancorpus(SIACSICOP1519)
+####Primera iteración  ####
+#para decidir que terminos eliminar y con que condiciones 
 d$ID <- seq.int(nrow(d))
 d$eliminate<-""
 d$comentarios<- ""
-
-####Proceso iterativo####
-#para decidir que terminos eliminar y con que condiciones 
-
 d[c(13,17,22,51,74,92,111,112,130,137,140,163,177,181,203,214,293,598),4] <- "Si"
 d[c(6,10,29,39,40,43,53,50,62,91,97,99,104,114,116,128,132,134,136,146),4] <- "Talvez"
-
-#### Comentarios ####
+### Comentarios ###
 d[6,5]<- "Corr thhn(0.48) & awg(0.42)"
 d[163,5]<- "Corr pizarra(0.69)"
 d[17,5]<- "Corr adhesiva(0.53)[eliminado] trasparente(0.31)"
 d[34,5]<- "Corr almohadilla(0.48)[eliminado] trodat(0.42)[eliminado] automatico(0.33)[eliminado condicionalmente] grm(0.31)[eliminado]"
 
-# Word Cloud (optional vizualization)
-set.seed(1234) 
-wordcloud(words = slice(d, -1:-155)$word, freq = slice(d, -1:-155)$freq, min.freq = 1,
-          max.words=200, random.order=FALSE, rot.per=0.35, 
-          colors=brewer.pal(8, "Dark2"))
-
-#### Eliminated data ####
+### Eliminated data ###
 techdata <- SIACSICOP1519[!grepl(paste(filter(d, eliminate == "Si")$word, collapse="|"), SIACSICOP1519$DESC_BIEN_SERVICIO),]
 ## Especiales
-
 #MARCADOR SIN RELOJ
 techdata <- techdata[!grepl('(^(?=.*\\bmarcador\\b)(?!.*\\breloj\\b))', techdata$DESC_BIEN_SERVICIO, perl = TRUE),] 
 
@@ -82,21 +69,19 @@ techdata <- techdata[!grepl('(^(?=.*\\bsello\\b)(?=.*\\bfechador\\b))', techdata
 techdata <- techdata[!grepl('(^(?=.*\\bsello\\b)(?=.*\\bprinter\\b))', techdata$DESC_BIEN_SERVICIO, perl = TRUE),] 
 techdata <- techdata[!grepl('(^(?=.*\\bsello\\b)(?=.*\\brepuesto\\b))', techdata$DESC_BIEN_SERVICIO, perl = TRUE),] 
 
+cleancorpus(techdata)
 
-#### Seccion de trabajo ####
+#### Seccion de trabajo---- opcionales ####
+
 term <- "tinta"
 Check <- techdata[grepl(term, techdata$DESC_BIEN_SERVICIO, perl = TRUE),] # check words in database
 Check <- as.data.frame(Check$DESC_BIEN_SERVICIO)
 findAssocs(dtm, terms = term, corlimit = 0.3)
 
-##### generate new corpus #####
-corpus <- VCorpus(VectorSource(techdata$DESC_BIEN_SERVICIO))
-toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x)) #Eliminar caracteres especiales
-corpus <- tm_map(corpus, toSpace, "/")#Eliminar caracteres especiales
-corpus <- tm_map(corpus, toSpace, "@")#Eliminar caracteres especiales
-corpus <- tm_map(corpus, toSpace, "\\|")#Eliminar caracteres especiales
-corpus <- tm_map(corpus, removeNumbers) # Remover numeros
-corpus <- tm_map(corpus, removeWords, stopwords("spanish")) # Remover stopwords de español
-corpus <- tm_map(corpus, removePunctuation) # Remove puntuación
-corpus <- tm_map(corpus, stripWhitespace) # Eliminate extra white spaces
-dtm <- TermDocumentMatrix(corpus)
+# Word Cloud (optional vizualization)
+set.seed(1234) 
+wordcloud(words = slice(d, -1:-155)$word, freq = slice(d, -1:-155)$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+
