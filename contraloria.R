5eef51a48d4c9ef7d3bd5b501bf8aac460b8e60a
caresiri <carlos.r.siri@gmail.com>
2019-10-04 21:50:21 -0600

    cambios para no borrar lineas, solo categorizarlas

--- contraloria.R
+++ contraloria.R
@@ -73,8 +73,8 @@ head(d, 10) #display top 10
 d$ID <- seq.int(nrow(d))
 d$eliminate<-""
 d$comentarios<- ""
-d[c(13,17,22,51,74,92,111,112,130,137,140,163,177,181,203,214,293,598),4] <- "Si"
-d[c(6,10,29,39,40,43,53,50,62,91,97,99,104,114,116,128,132,134,136,146),4] <- "Talvez"
+d[c(17,22,51,74,92,111,112,130,137,140,163,177,181,203,214,293,598),4] <- "Si"
+d[c(13,6,10,29,39,40,43,53,50,62,91,97,99,104,114,116,128,132,134,136,146),4] <- "Talvez"
 ### Comentarios ###
 d[6,5]<- "Corr thhn(0.48) & awg(0.42)"
 d[163,5]<- "Corr pizarra(0.69)"
@@ -82,22 +82,19 @@ d[17,5]<- "Corr adhesiva(0.53)[eliminado] trasparente(0.31)"
 d[34,5]<- "Corr almohadilla(0.48)[eliminado] trodat(0.42)[eliminado] automatico(0.33)[eliminado condicionalmente] grm(0.31)[eliminado]"
 
 ### Eliminated data ###
-techdata <- SIACSICOP1519[!grepl(paste(filter(d, eliminate == "Si")$word, collapse="|"), SIACSICOP1519$DESC_BIEN_SERVICIO),]
+SIACSICOP1519$tech <- ifelse(grepl(paste(filter(d, eliminate == "Si")$word, collapse="|"), SIACSICOP1519$DESC_BIEN_SERVICIO, perl = TRUE),"No","No_Se")
 ## Especiales
-#MARCADOR SIN RELOJ
-techdata <- techdata[!grepl('(^(?=.*\\bmarcador\\b)(?!.*\\breloj\\b))', techdata$DESC_BIEN_SERVICIO, perl = TRUE),] 
-
-#Hay algunas descripciones que tienen la palabra sello que podrian ser tecnicas
-techdata <- techdata[!grepl('(^(?=.*\\bsello\\b)(?=.*\\bautomatico\\b))', techdata$DESC_BIEN_SERVICIO, perl = TRUE),] 
-techdata <- techdata[!grepl('(^(?=.*\\bsello\\b)(?=.*\\bfechador\\b))', techdata$DESC_BIEN_SERVICIO, perl = TRUE),] 
-techdata <- techdata[!grepl('(^(?=.*\\bsello\\b)(?=.*\\bprinter\\b))', techdata$DESC_BIEN_SERVICIO, perl = TRUE),] 
-techdata <- techdata[!grepl('(^(?=.*\\bsello\\b)(?=.*\\brepuesto\\b))', techdata$DESC_BIEN_SERVICIO, perl = TRUE),] 
-
+SIACSICOP1519$tech[grepl('(^(?=.*\\bmarcador\\b)(?!.*\\breloj\\b))', SIACSICOP1519$DESC_BIEN_SERVICIO, perl = TRUE)] = "No"#MARCADOR SIN RELOJ
+SIACSICOP1519$tech[grepl('(^(?=.*\\bsello\\b)(?=.*\\bautomatico\\b))', SIACSICOP1519$DESC_BIEN_SERVICIO, perl = TRUE)] = "No"
+SIACSICOP1519$tech[grepl('(^(?=.*\\bsello\\b)(?=.*\\bfechador\\b))', SIACSICOP1519$DESC_BIEN_SERVICIO, perl = TRUE)] = "No"
+SIACSICOP1519$tech[grepl('(^(?=.*\\bsello\\b)(?=.*\\bprinter\\b))', SIACSICOP1519$DESC_BIEN_SERVICIO, perl = TRUE)] = "No"
+SIACSICOP1519$tech[grepl('(^(?=.*\\bsello\\b)(?=.*\\brepuesto\\b))', SIACSICOP1519$DESC_BIEN_SERVICIO, perl = TRUE)] = "No"
 
 ####Segunda Iteración####
 d0<-d
 #cleancorpus(techdata) FUNCTION NOT WORKING DO IT MANUALLY FOR NOW
-
+techdata <- SIACSICOP1519 %>% filter(tech=="No_Se")
+save(techdata, file = "techdata.RData")
 corpus <- VCorpus(VectorSource(techdata$DESC_BIEN_SERVICIO)) #Generación de corpus de Descripción de Bienes y Servicios del SIAC
 toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x)) #Eliminar caracteres especiales
 corpus <- tm_map(corpus, toSpace, "/")#Eliminar caracteres especiales
@@ -120,14 +117,20 @@ d <- d[order(-d[,2]),] # order by frequency
 rownames(d) <- NULL # reset column order
 
 #### Eliminated data ####
-d[c(602),5] <- "Si"
-#Papel e impresora puede ser tecnico
-techdata <- techdata[!grepl('(^(?=.*\\bpapel\\b)(?!.*\\bimpresora\\b))', techdata$DESC_BIEN_SERVICIO, perl = TRUE),] 
-techdata <- techdata[!grepl(paste(filter(d, eliminate == "Si")$word, collapse="|"), techdata$DESC_BIEN_SERVICIO),]
+d[c(196,225,298,602,247,975,1748,1812),5] <- "Si"
+d[c(1,2,3,4,5,6,7,8,9,10,11,13,14,16,17,18,19,21,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,41,42,43,44,45,46,47,49,50,51,52,54),5] <- "No"
+d[c(15,20,39,40,48,152),5] <- "Talvez"
 
+SIACSICOP1519$tech[grepl('(^(?=.*\\bpapel\\b)(?!.*\\bimpresora\\b))', SIACSICOP1519$DESC_BIEN_SERVICIO, perl = TRUE)] = "No" # dejar Papel e impresora puede ser tecnico
+SIACSICOP1519$tech[grepl('(^(?=.*\\bplastico\\b)(?=.*\\badhesivo\\b))', SIACSICOP1519$DESC_BIEN_SERVICIO, perl = TRUE)] = "No"
+SIACSICOP1519$tech[grepl('(^(?=.*\\bplastico\\b)(?=.*\\brollo\\b))', SIACSICOP1519$DESC_BIEN_SERVICIO, perl = TRUE)] = "No"
+SIACSICOP1519$tech[grepl('(^(?=.*\\bplastico\\b)(?=.*\\bforro\\b))', SIACSICOP1519$DESC_BIEN_SERVICIO, perl = TRUE)] = "No"
+SIACSICOP1519$tech[grepl('grepl(paste(filter(d, eliminate == "Si")$word, collapse="|")', SIACSICOP1519$DESC_BIEN_SERVICIO, perl = TRUE)] = "No"
+
+#techdata <- techdata[!grepl(paste(filter(d, eliminate == "Si")$word, collapse="|"), techdata$DESC_BIEN_SERVICIO),]
 
 #### Seccion de trabajo---- opcionales ####
-term <- "papel"
+term <- "led"
 Check <- techdata[grepl(term, techdata$DESC_BIEN_SERVICIO, perl = TRUE),] # check words in database
 Check <- as.data.frame(Check$DESC_BIEN_SERVICIO)
 findAssocs(dtm, terms = term, corlimit = 0.2)
