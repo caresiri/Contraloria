8dfe1530b6b404581eb4402f89e340b99b4ca1b9
caresiri <carlos.r.siri@gmail.com>
2019-12-24 09:56:30 -0600

    code to send

--- Editor.R
+++ Editor.R
@@ -10,37 +10,26 @@ library(tidyr)
 library(widyr)
 library(ggplot2)
 library(readxl)
-
 setwd("/Volumes/GoogleDrive/My Drive/INCAE Work Drive/Investigador/Proyectos/Contraloria/Text Mining/workstudy")
-library(readxl)
-SIACSICOP1519 <- read_excel("adjudicacionesycompras.xlsx", 
-                                           col_types = c("text", "text", "text", 
-                                                                    "text", "text", "text", "text", "numeric", 
-                                                                    "numeric", "numeric", "text", "text", 
-                                                                    "text", "text", "numeric", "numeric", 
-                                                                    "date", "numeric", "text", "text", 
-                                                                    "text", "text", "text", "text", "numeric", 
-                                                                    "date", "text", "text", "numeric", 
-                                                                    "date", "text", "numeric", "numeric", 
-                                                                    "text", "text", "text", "text", "text", 
-                                                                    "numeric", "numeric"))
-
 #### Procedimiento ####
-#  - instalar paquetes de nuevo -  son unos nuevos que funcionan para todos 
-#  - guardar SIACSICOP1519.Rda, d.csv, mining.xlsx, y spanish_stop_words.Rds en su folder de trabajo
-#  - 1.     Revisión de tabla “d” diario para ver palabras por categorizar. 
-#  - 2.     Filtrar por letra asignada
-#  - 3.     Comenzar por letra asignada a revisar la tabla (Si terminan con sus letras asignadas, pueden seguir, la duplicación en este caso es positiva, el lunes hablamos más de esto)
-#Letras asignadas
-#A-C Andrés Emilio Martínez Landaverde
-#D-F Pablo Argueta Juárez
-#G-I Fabio Darío Zarza Vera 
-#J-L Juan Pablo Espinoza Cabrera
-#M-O Laura Rebeca Monge Alvarado
-#P-R Franco Pérez
-#S–U Rubén Ramírez
-#V-Z Vladimir Eduardo Luna
-
+#library(readxl)
+#SIACSICOP1519 <- read_excel("adjudicacionesycompras.xlsx", 
+#                                          col_types = c("text", "text", "text", 
+#                                                                  "text", "text", "text", "text", "numeric", 
+#                                                                 "numeric", "numeric", "text", "text", 
+#                                                                "text", "text", "numeric", "numeric", 
+#                                                               "date", "numeric", "text", "text", 
+#                                                              "text", "text", "text", "text", "numeric", 
+#                                                             "date", "text", "text", "numeric", 
+#                                                            "date", "text", "numeric", "numeric", 
+#                                                           "text", "text", "text", "text", "text", 
+#                                                          "numeric", "numeric"))
+#1.	Bajar R y R Studio (Instrucciones en anexo)
+#2.	instalar paquetes de nuevo -  son unos nuevos que funcionan para todos
+#3.	guardar SIACSICOP1519.Rda, categorización.xlsx, y spanish_stop_words.Rds en su folder de trabajo
+#4.	Abrir categorización.xlsx
+#5.	Filtrar por letra asignada
+#6.	Ver video explicativo
 
 #Letras asignadas
 #A-B Ximena Rodriguez
@@ -63,11 +52,24 @@ SIACSICOP1519 <- read_excel("adjudicacionesycompras.xlsx",
 #8. Categorizar las palabras según criterio en la columna [Eliminate] como {No, Tal vez, Si}
 #9. Categorizar las palabras según criterio en la columna [CAT_BIEN_SERVICIO]
 
+#### Letras de categorizacion tech o no tech
+#Letras asignadas
+#A-C Andrés Emilio Martínez Landaverde
+#D-F Pablo Argueta Juárez
+#G-I Fabio Darío Zarza Vera 
+#J-L Juan Pablo Espinoza Cabrera
+#M-O Laura Rebeca Monge Alvarado
+#P-R Franco Pérez
+#S–U Rubén Ramírez
+#V-Z Vladimir Eduardo Luna
+
+
 ####Load data####
 load("SIACSICOP1519.Rda")
 spanish_stop_words <- readRDS("spanish_stop_words.Rds")
 reduccion <- SIACSICOP1519 %>%
-  filter(tech == "No_Se") 
+  filter(tech == "Si",
+         is.na(CAT_BIEN_SERVICIO)) 
 #### Generación de Corpus ####
 corpus <- reduccion %>%
   mutate(text = gsub(x = DESC_BIEN_SERVICIO, pattern = "[0-9]+|[[:punct:]]|\\(.*\\)", replacement = "")) %>%
@@ -85,14 +87,13 @@ word_pairs <- corpus  %>%
   pairwise_count(word, id, sort = TRUE)
 word_pairs
 #### Correlate among sections ####
-word_1 ="color"
+word_1 ="adobe"
 word_cors <- corpus %>%
   group_by(word) %>%
   filter(n() >= 20) %>%
   pairwise_cor(word, id, sort = TRUE) %>%
   filter(item1 == word_1)
 #### Correlate Specific terms ####
-
 word_cors %>%
   filter(item1 %in% c(word_1)) %>%
   group_by(item1) %>%
